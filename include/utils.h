4ffe91994f3a8b87a1d1d0d6c4e8717dc6c1b170
giovitafu <210045640+giovitafu@users.noreply.github.com>
2025-05-03 13:16:50 +0200

    Add utils.h and a macro to check bad allocations

---
+++ include/utils.h
@@ -0,0 +1,23 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <stdio.h>
+
+#define CHECK_NULL(ptr) do { \
+    if ((ptr) == NULL) { \
+        perror("ERROR: %s is NULL at %s:%d\n", #ptr, __FILE__, __LINE__); \
+        exit(1); \
+    } \
+} while (0)
+
+// date format dd/mm/aaaa
+
+/*
+    The compare_date function takes two date
+    Return -1 if the first date is previous to the second
+    Return 0 if the first date and the second are equals
+    Return 1 if the first date is after the second
+*/
+int compare_date(const char* date_one, const char* date_two);
+
+#endif
