89f761ae7d9994725688337e0dcbcf6910998ae9
AleeGredi <alessandrotangredi06@gmail.com>
2025-05-11 13:25:33 +0200

    Add save and load functions for user
    Implemented save_user_to_file and load_user_from_file functions in user.c
    to enable reading/writing user data in CSV format (comma-separated)

--- source/user.c
+++ source/user.c
@@ -1,4 +1,5 @@
 #include "user.h"
+#include "subscription.h"
 #include "utils.h"
 
 #include <stdio.h>
@@ -69,6 +70,53 @@ const char* get_user_password(user_ptr user) {
 
 subscription_ptr get_user_subscription(user_ptr user) {
     return user->subscription;
+}
+
+void save_user_to_file(user_ptr user, FILE* f) {
+    fprintf(f, "%u,%s,%s,%s,%s,%u,%s,%s\n",
+        user->id,
+        user->CF,
+        user->first_name,
+        user->last_name,
+        user->password,
+        get_subscription_id(user->subscription),
+        get_subscription_start_date(user->subscription),
+        get_subscription_end_date(user->subscription)
+    );
+}
+
+user_ptr load_user_from_file(FILE* f) {
+    if (!f) return NULL;
+
+    char line[512];
+    if (!fgets(line, sizeof(line), f)) return NULL;
+
+    uint16_t id, sub_id;
+    char CF[50], first_name[50], last_name[50], password[50];
+    char start_date[11], end_date[11];
+
+    int parsed = sscanf(
+        line,
+        "%hu,%49[^,],%49[^,],%49[^,],%49[^,],%hu,%10[^,],%10[^,\n]",
+        &id,
+        CF,
+        first_name,
+        last_name,
+        password,
+        &sub_id,
+        start_date,
+        end_date
+    );
+
+    if (parsed != 8) {
+        fprintf(stderr, "Errore nel parsing della riga utente.\n");
+        return NULL;
+    }
+
+    subscription_ptr sub = create_subscription(sub_id, start_date, end_date);
+    if (!sub) return NULL;
+
+    return create_user(id, CF, first_name, last_name, password, sub);
 }
 
 void print_user(user_ptr user) {
