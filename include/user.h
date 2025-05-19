9626fd79209c633e845d8d47567478231ed06e7d
AleeGredi <alessandrotangredi06@gmail.com>
2025-05-19 17:38:10 +0200

    Add username field and documentation to user module(.h and .c files)
    Added the username field to the user struct and all related functions.
    Documented all functions in user.h with clear descriptions, parameters, pre-/post-conditions, and return values.

--- include/user.h
+++ include/user.h
@@ -7,25 +7,226 @@
 
 typedef struct user *user_ptr;
 
+/*
+    Creates a new user object with the specified parameters.
+
+    Parameters:
+        id: unique identifier for the user.
+        CF: codice fiscale (tax code), null-terminated string.
+        first_name: user's first name, null-terminated string.
+        last_name: user's last name, null-terminated string.
+        username: user's login username, null-terminated string.
+        password: user's login password, null-terminated string.
+        subscription: pointer to a valid subscription object.
+
+    Pre-conditions:
+        All strings must be valid (not NULL).
+        subscription must not be NULL.
+
+    Post-conditions:
+        A new user object is allocated and initialized.
+
+    Returns:
+        user_ptr: pointer to the newly created user.
+*/
 user_ptr create_user(
     uint16_t id,
     char* CF,
     char* first_name,
     char* last_name,
+    char* username,
     char* password,
     subscription_ptr subscription    
 );
 
+/*
+    Returns the user's ID.
+
+    Parameters:
+        user: pointer to the user object.
+
+    Pre-conditions:
+        user must not be NULL.
+
+    Post-conditions:
+        None.
+
+    Returns:
+        uint16_t: user's ID.
+*/
 uint16_t get_user_id(user_ptr user);
+
+/*
+    Returns the user's codice fiscale (CF).
+
+    Parameters:
+        user: pointer to the user object.
+
+    Pre-conditions:
+        user must not be NULL.
+
+    Post-conditions:
+        None.
+
+    Returns:
+        const char*: pointer to the CF string.
+*/
 const char* get_user_CF(user_ptr user);
+
+/*
+    Returns the user's first name.
+
+    Parameters:
+        user: pointer to the user object.
+
+    Pre-conditions:
+        user must not be NULL.
+
+    Post-conditions:
+        None.
+
+    Returns:
+        const char*: pointer to the first name string.
+*/
 const char* get_user_first_name(user_ptr user);
+
+/*
+    Returns the user's last name.
+
+    Parameters:
+        user: pointer to the user object.
+
+    Pre-conditions:
+        user must not be NULL.
+
+    Post-conditions:
+        None.
+
+    Returns:
+        const char*: pointer to the last name string.
+*/
 const char* get_user_last_name(user_ptr user);
+
+/*
+    Returns the user's username.
+
+    Parameters:
+        user: pointer to the user object.
+
+    Pre-conditions:
+        user must not be NULL.
+
+    Post-conditions:
+        None.
+
+    Returns:
+        const char*: pointer to the username string.
+*/
+const char* get_user_username(user_ptr user);
+
+/*
+    Returns the user's password.
+
+    Parameters:
+        user: pointer to the user object.
+
+    Pre-conditions:
+        user must not be NULL.
+
+    Post-conditions:
+        None.
+
+    Returns:
+        const char*: pointer to the password string.
+*/
 const char* get_user_password(user_ptr user);
+
+/*
+    Returns the user's subscription.
+
+    Parameters:
+        user: pointer to the user object.
+
+    Pre-conditions:
+        user must not be NULL.
+
+    Post-conditions:
+        None.
+
+    Returns:
+        subscription_ptr: pointer to the user's subscription.
+*/
 subscription_ptr get_user_subscription(user_ptr user);
 
+/*
+    Prints user information to standard output.
+
+    Parameters:
+        user: pointer to the user object.
+
+    Pre-conditions:
+        user must not be NULL.
+
+    Post-conditions:
+        None.
+
+    Returns:
+        None.
+*/
 void print_user(user_ptr user);
+
+/*
+    Saves the user to a file in binary format.
+
+    Parameters:
+        user: pointer to the user object.
+        f: pointer to an open file for writing.
+
+    Pre-conditions:
+        user and f must not be NULL.
+        File must be open for writing.
+
+    Post-conditions:
+        User data is written to the file.
+
+    Returns:
+        None.
+*/
 void save_user_to_file(user_ptr user, FILE* f);
+
+/*
+    Loads a user from a file in binary format.
+
+    Parameters:
+        f: pointer to an open file for reading.
+
+    Pre-conditions:
+        f must not be NULL.
+        File must contain valid user data.
+
+    Post-conditions:
+        A new user object is created and returned.
+
+    Returns:
+        user_ptr: pointer to the loaded user object.
+*/
 user_ptr load_user_from_file(FILE* f);
+
+/*
+    Frees all memory associated with the user.
+
+    Parameters:
+        user: pointer to the user object to delete.
+
+    Pre-conditions:
+        user must not be NULL.
+
+    Post-conditions:
+        All allocated memory for the user and its fields is released.
+
+    Returns:
+        None.
+*/
 void delete_user(user_ptr user);
 
 #endif
--- source/user.c
+++ source/user.c
@@ -11,15 +11,39 @@ struct user {
     char* CF;
     char* first_name;
     char* last_name;
+    char* username;
     char* password;
     subscription_ptr subscription;
 };
 
+/*
+    Allocates and initializes a new user object by allocating its strings
+    and setting its fields.
+
+    Parameters:
+        id: unique user identifier.
+        CF: user's codice fiscale (null-terminated string).
+        first_name: user's first name (null-terminated string).
+        last_name: user's last name (null-terminated string).
+        username: user's login username (null-terminated string).
+        password: user's login password (null-terminated string).
+        subscription: pointer to a valid subscription object.
+
+    Pre-conditions:
+        All string parameters and subscription must not be NULL.
+
+    Post-conditions:
+        Returns a pointer to a newly allocated user object with deep copies of the strings.
+
+    Returns:
+        user_ptr: pointer to the initialized user object.
+*/
 user_ptr create_user(
     uint16_t id,
     char* CF,
     char* first_name,
     char* last_name,
+    char* username,
     char* password,
     subscription_ptr subscription    
 ){
@@ -39,6 +63,10 @@ user_ptr create_user(
     CHECK_NULL(new_user->last_name);
     strcpy(new_user->last_name, last_name);
 
+    new_user->username = malloc(strlen(username) + 1);
+    CHECK_NULL(new_user->username);
+    strcpy(new_user->username, username);
+
     new_user->password = malloc(strlen(password) + 1);
     CHECK_NULL(new_user->password);
     strcpy(new_user->password, password);
@@ -48,36 +76,142 @@ user_ptr create_user(
     return new_user;
 }
 
+/*
+    Returns the ID of the user.
+
+    Parameters:
+        user: pointer to a valid user object.
+
+    Pre-conditions:
+        user must not be NULL.
+
+    Returns:
+        uint16_t: user ID.
+*/
 uint16_t get_user_id(user_ptr user) {
     return user->id;
 }
 
+/*
+    Returns the codice fiscale of the user.
+
+    Parameters:
+        user: pointer to a valid user object.
+
+    Pre-conditions:
+        user must not be NULL.
+
+    Returns:
+        const char*: pointer to the user's CF string.
+*/
 const char* get_user_CF(user_ptr user) {
     return user->CF;
 }
 
+/*
+    Returns the first name of the user.
+
+    Parameters:
+        user: pointer to a valid user object.
+
+    Pre-conditions:
+        user must not be NULL.
+
+    Returns:
+        const char*: pointer to the user's first name.
+*/
 const char* get_user_first_name(user_ptr user) {
     return user->first_name;
 }
 
+/*
+    Returns the last name of the user.
+
+    Parameters:
+        user: pointer to a valid user object.
+
+    Pre-conditions:
+        user must not be NULL.
+
+    Returns:
+        const char*: pointer to the user's last name.
+*/
 const char* get_user_last_name(user_ptr user) {
     return user->last_name;
 }
 
+/*
+    Returns the username of the user.
+
+    Parameters:
+        user: pointer to a valid user object.
+
+    Pre-conditions:
+        user must not be NULL.
+
+    Returns:
+        const char*: pointer to the user's username.
+*/
+const char* get_user_username(user_ptr user) {
+    return user->username;
+}
+
+/*
+    Returns the password of the user.
+
+    Parameters:
+        user: pointer to a valid user object.
+
+    Pre-conditions:
+        user must not be NULL.
+
+    Returns:
+        const char*: pointer to the user's password.
+*/
 const char* get_user_password(user_ptr user) {
     return user->password;
 }
 
+/*
+    Returns the subscription associated with the user.
+
+    Parameters:
+        user: pointer to a valid user object.
+
+    Pre-conditions:
+        user must not be NULL.
+
+    Returns:
+        subscription_ptr: pointer to the user's subscription.
+*/
 subscription_ptr get_user_subscription(user_ptr user) {
     return user->subscription;
 }
 
+/*
+    Saves user data to a file in CSV format.
+
+    Parameters:
+        user: pointer to the user object.
+        f: pointer to an open file for writing.
+
+    Pre-conditions:
+        user and f must not be NULL.
+        File must be open in write or append mode.
+
+    Post-conditions:
+        User data is written to the file in CSV format.
+
+    Returns:
+        None.
+*/
 void save_user_to_file(user_ptr user, FILE* f) {
-    fprintf(f, "%u,%s,%s,%s,%s,%u,%s,%s\n",
+    fprintf(f, "%u,%s,%s,%s,%s,%s,%u,%s,%s\n",
         user->id,
         user->CF,
         user->first_name,
         user->last_name,
+        user->username,
         user->password,
         get_subscription_id(user->subscription),
         get_subscription_start_date(user->subscription),
@@ -85,6 +219,22 @@ void save_user_to_file(user_ptr user, FILE* f) {
     );
 }
 
+/*
+    Loads a user from a file containing CSV-formatted data.
+
+    Parameters:
+        f: pointer to an open file for reading.
+
+    Pre-conditions:
+        f must not be NULL.
+        File must contain a valid line of user data.
+
+    Post-conditions:
+        A new user object is created and returned.
+
+    Returns:
+        user_ptr: pointer to the loaded user object, or NULL on error.
+*/
 user_ptr load_user_from_file(FILE* f) {
     if (!f) return NULL;
 
@@ -92,23 +242,24 @@ user_ptr load_user_from_file(FILE* f) {
     if (!fgets(line, sizeof(line), f)) return NULL;
 
     uint16_t id, sub_id;
-    char CF[50], first_name[50], last_name[50], password[50];
+    char CF[50], first_name[50], last_name[50], username[50], password[50];
     char start_date[11], end_date[11];
 
     int parsed = sscanf(
         line,
-        "%hu,%49[^,],%49[^,],%49[^,],%49[^,],%hu,%10[^,],%10[^,\n]",
+        "%hu,%49[^,],%49[^,],%49[^,],%49[^,],%49[^,],%hu,%10[^,],%10[^,\n]",
         &id,
         CF,
         first_name,
         last_name,
+        username,
         password,
         &sub_id,
         start_date,
         end_date
     );
 
-    if (parsed != 8) {
+    if (parsed != 9) {
         fprintf(stderr, "Errore nel parsing della riga utente.\n");
         return NULL;
     }
@@ -116,23 +267,52 @@ user_ptr load_user_from_file(FILE* f) {
     subscription_ptr sub = create_subscription(sub_id, start_date, end_date);
     if (!sub) return NULL;
 
-    return create_user(id, CF, first_name, last_name, password, sub);
+    return create_user(id, CF, first_name, last_name, username, password, sub);
 }
 
+/*
+    Prints detailed user information to standard output.
+
+    Parameters:
+        user: pointer to a valid user object.
+
+    Pre-conditions:
+        user must not be NULL.
+
+    Returns:
+        None.
+*/
 void print_user(user_ptr user) {
     printf("User ID     : %u\n", user->id);
     printf("CF          : %s\n", user->CF);
     printf("First Name  : %s\n", user->first_name);
     printf("Last Name   : %s\n", user->last_name);
+    printf("Username    : %s\n", user->username);
     printf("Password    : %s\n", user->password);
     printf("Subscription:\n");
     print_subscription(user->subscription);
 }
 
+/*
+    Frees all memory associated with a user object.
+
+    Parameters:
+        user: pointer to the user object to delete.
+
+    Pre-conditions:
+        user must not be NULL.
+
+    Post-conditions:
+        All memory allocated for the user and its fields is released.
+
+    Returns:
+        None.
+*/
 void delete_user(user_ptr user) {
     free(user->CF);
     free(user->first_name);
     free(user->last_name);
+    free(user->username);
     free(user->password);
 
     delete_subscription(user->subscription);
